8ec797f07cfc15a35793c0728014733d73906bf8
Anna Tasheva <anna.tasheva90@gmail.com>
2022-05-13 11:27:55 +0100

    Added final features to the project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
@@ -11,13 +11,26 @@ function App() {
       <div>
         <Navigation />
 
-        <Route path='/:type/:id'>
-          <PetDetailsPage />
-        </Route>
+        <Switch>
+          <Route path='/:type/:id'>
+            <PetDetailsPage />
+          </Route>
 
-        <Route path='/:type?'>
-          <HomePage />
-        </Route>
+          <Route path='/search'>
+            <SearchPage />
+          </Route>
+
+          <Route path='/pet-details-not-found'>
+            <PetDetailsNotFound />
+          </Route>
+
+          <Route path='/:type?'>
+            <HomePage />
+          </Route>
+
+
+        </Switch>
+
 
       </div>
     </Router>
